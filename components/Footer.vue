6c1f0555c242ae4ed1caedf49cc4fb0206dcf2c0
Rodney Dennis <rodneydennisjr@gmail.com>
2019-03-19 06:42:58 -0500

    Move AC tracking to index

--- components/Footer.vue
+++ components/Footer.vue
@@ -13,15 +13,6 @@ export default {
       year: new Date().getFullYear(),
     }
   },
-
-  created() {
-    (function(e,t,o,n,p,r,i){e.prismGlobalObjectAlias=n;e.pgo=e.pgo||function(){(e.pgo.q=e.pgo.q||[]).push(arguments)};e.pgo.l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://prism.app-us1.com/prism.js","pgo");
-
-    pgo('setAccount', '89472534');
-    pgo('setTrackByDefault', true);
-
-    pgo('process');
-  }
 }
 </script>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -15,6 +15,16 @@
 
       gtag('config', 'UA-133879449-1');
     </script>
+
+    <!-- ActiveCampaign Tracking -->
+    <script type="text/javascript">
+      (function(e,t,o,n,p,r,i){e.prismGlobalObjectAlias=n;e.pgo=e.pgo||function(){(e.pgo.q=e.pgo.q||[]).push(arguments)};e.pgo.l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://prism.app-us1.com/prism.js","pgo");
+
+      pgo('setAccount', '89472534');
+      pgo('setTrackByDefault', true);
+
+      pgo('process');
+    </script>
   </div>
 </template>
 
