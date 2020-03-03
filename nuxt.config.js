76d589956e6ae2dcb3a02fc98084d1589c4232ff
Rodney Dennis <rodneydennisjr@gmail.com>
2020-03-02 19:55:41 -0600

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ export default {
   head: {
     meta: [
       { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-  },
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
+    ]
+  }
 };
