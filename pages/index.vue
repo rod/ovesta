0d33b0eddad12d377d366f4f8f68e02888d9a778
Rodney Dennis <rodneydennisjr@gmail.com>
2020-03-02 19:54:05 -0600

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,3 @@
 <template>
-  <div class="intro">
-    {{ site }}
-  </div>
+  <div>rod was here</div>
 </template>
-
-<script>
-export default {
-  layout: 'default',
-
-  data() {
-    return {
-      bodycopy: '',
-    }
-  }
-}
-</script>
-
-<style>
-.intro {
-  font-size: var(--font-size-6);
-  font-weight: 700;
-  padding: 1.125rem;
-  text-transform: lowercase;
-}
-</style>
