6f147ce3eafdd6000f1df33891fa563b8cbabfa6
Rodney Dennis <rodneydennisjr@gmail.com>
2020-03-29 10:33:22 -0500

    Add head

---
+++ nuxt.config.js
@@ -0,0 +1,14 @@
+export default {
+  head: {
+    titleTemplate: "Rod Dennis",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Making things as simple as possible. But not simpler."
+      }
+    ]
+  }
+};
